ed8edb25f3b27fc6184bd2a02695b2b4b8b8284e
Apk <146819953+7NRVHXY@users.noreply.github.com>
2023-10-31 23:02:47 +0400

    created4

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace elseifheight
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int height = 170;    
+            bool isMale = false; 
+            if (height>180 ) 
+            {
+                Console.WriteLine("arxa sira");
+            }
+            else if (height>170 && height < 180)
+            {
+                Console.WriteLine("orta sira");
+            }
+            else //en sonda bize zaten 170den kicikler lazimdi ona gore sert yazmaga ehtiyac yoxdu
+            {
+                Console.WriteLine("on sira "); 
+            }
+            if (isMale)
+            {
+                Console.WriteLine("sag teref");
+            }
+            else
+            {
+                Console.WriteLine("sol teref");
+            }
+        }
+    }
+}
